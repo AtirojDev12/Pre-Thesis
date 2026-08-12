542ea3f1907f08622d17a329571f191d7f2becb6
kong1735 <kongkidakorn.chu@gmail.com>
2026-08-12 14:05:43 +0700

    ระบบเก็บไอเท็ม (Finish (maybe)) ยังไม่ไปใส่ Item ทั้งหมด

--- Assets/_Script/SaveSystem/SaveManager.cs
+++ Assets/_Script/SaveSystem/SaveManager.cs
@@ -4,17 +4,21 @@ using System.Security.Cryptography;
 using System.Text;
 using UnityEngine;
 
+
 public static class SaveManager
 {
     private const string FILE_NAME = "playerdata.sav";
 
+
     public static SaveData Current { get; private set; }
+
 
     // กุญแจแบ่งเป็นชิ้น ๆ แล้วค่อยประกอบตอนรัน จะได้ไม่มี String เดี่ยว ๆ ให้ grep เจอง่าย ๆ
     private static readonly byte[] keyPart1 = { 0x4A, 0x2F, 0x8C, 0x11, 0x9D, 0x3E, 0x77, 0x60 };
     private static readonly byte[] keyPart2 = { 0xB4, 0x05, 0xE2, 0x9A, 0x1C, 0x88, 0x4F, 0x33 };
     private static readonly byte[] keyPart3 = { 0x6D, 0xF1, 0x2A, 0x00, 0x7C, 0x9B, 0x55, 0xE8 };
     private static readonly byte[] keyPart4 = { 0x91, 0x3D, 0xC6, 0x4E, 0x0F, 0xA7, 0x28, 0xBB };
+
 
     private static byte[] GetKey()
     {
@@ -26,7 +30,9 @@ public static class SaveManager
         return key;
     }
 
+
     private static string SavePath => Path.Combine(Application.persistentDataPath, FILE_NAME);
+
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Init()
@@ -35,19 +41,23 @@ public static class SaveManager
         Debug.Log("Save loaded. Currency = " + Current.currency);
     }
 
+
     public static void SaveToDisk()
     {
         string json = JsonUtility.ToJson(Current);
         byte[] plainBytes = Encoding.UTF8.GetBytes(json);
+
 
         using (Aes aes = Aes.Create())
         {
             aes.Key = GetKey();
             aes.GenerateIV();
 
+
             using (ICryptoTransform encryptor = aes.CreateEncryptor())
             {
                 byte[] encryptedBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+
 
                 using (FileStream fs = new FileStream(SavePath, FileMode.Create))
                 {
@@ -57,8 +67,10 @@ public static class SaveManager
             }
         }
 
+
         Debug.Log("Saved to: " + SavePath);
     }
+
 
     private static SaveData Load()
     {
@@ -68,19 +80,23 @@ public static class SaveManager
             return new SaveData();
         }
 
+
         try
         {
             byte[] fileBytes = File.ReadAllBytes(SavePath);
+
 
             byte[] iv = new byte[16];
             byte[] encryptedBytes = new byte[fileBytes.Length - 16];
             Buffer.BlockCopy(fileBytes, 0, iv, 0, 16);
             Buffer.BlockCopy(fileBytes, 16, encryptedBytes, 0, encryptedBytes.Length);
 
+
             using (Aes aes = Aes.Create())
             {
                 aes.Key = GetKey();
                 aes.IV = iv;
+
 
                 using (ICryptoTransform decryptor = aes.CreateDecryptor())
                 {
@@ -97,10 +113,12 @@ public static class SaveManager
         }
     }
 
+
     public static void AddCurrency(int amount)
     {
         Current.currency += amount;
     }
+
 
     public static bool SpendCurrency(int amount)
     {
@@ -108,4 +126,63 @@ public static class SaveManager
         Current.currency -= amount;
         return true;
     }
+
+
+    public static void AddConsumable(string itemID, int amount)
+    {
+        var item = Current.consumables.Find(x => x.itemID == itemID);
+        if (item != null)
+        {
+            item.quantity += amount;
+        }
+        else
+        {
+            Current.consumables.Add(new ConsumableItemData(itemID, amount));
+        }
+    }
+
+
+    public static bool ConsumeItem(string itemID, int amount = 1)
+    {
+        var item = Current.consumables.Find(x => x.itemID == itemID);
+        if (item != null && item.quantity >= amount)
+        {
+            item.quantity -= amount;
+            if (item.quantity <= 0)
+            {
+                Current.consumables.Remove(item);
+            }
+            return true;
+        }
+        return false;
+    }
+
+
+    public static int GetConsumableQuantity(string itemID)
+    {
+        var item = Current.consumables.Find(x => x.itemID == itemID);
+        return item != null ? item.quantity : 0;
+    }
+
+
+    public static void AddPermanentItem(string itemID)
+    {
+        var item = Current.permanentItems.Find(x => x.itemID == itemID);
+        if (item != null)
+        {
+            item.isOwned = true;
+        }
+        else
+        {
+            Current.permanentItems.Add(new PermanentItemData(itemID, true));
+        }
+    }
+
+
+    public static bool HasPermanentItem(string itemID)
+    {
+        var item = Current.permanentItems.Find(x => x.itemID == itemID);
+        return item != null && item.isOwned;
+    }
 }
+
--- Assets/_Script/SaveSystem/SaveTester.cs
+++ Assets/_Script/SaveSystem/SaveTester.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 public class SaveTester : MonoBehaviour
 {
     [ContextMenu("Add 50 Currency")]
@@ -9,6 +10,7 @@ public class SaveTester : MonoBehaviour
         Debug.Log("Currency ตอนนี้: " + SaveManager.Current.currency);
     }
 
+
     [ContextMenu("Spend 20 Currency")]
     private void TestSpend()
     {
@@ -16,9 +18,42 @@ public class SaveTester : MonoBehaviour
         Debug.Log("Spend สำเร็จ: " + success + " | Currency ตอนนี้: " + SaveManager.Current.currency);
     }
 
+
+    [ContextMenu("Add Medkit (Consumable)")]
+    private void TestAddConsumable()
+    {
+        SaveManager.AddConsumable("medkit", 2);
+        Debug.Log("Added 2 Medkits. Total: " + SaveManager.GetConsumableQuantity("medkit"));
+    }
+
+
+    [ContextMenu("Use Medkit (Consumable)")]
+    private void TestUseConsumable()
+    {
+        bool success = SaveManager.ConsumeItem("medkit", 1);
+        Debug.Log("Used 1 Medkit: " + success + " | Total now: " + SaveManager.GetConsumableQuantity("medkit"));
+    }
+
+
+    [ContextMenu("Buy Flashlight (Permanent)")]
+    private void TestAddPermanent()
+    {
+        SaveManager.AddPermanentItem("flashlight");
+        Debug.Log("Bought Flashlight. Owned: " + SaveManager.HasPermanentItem("flashlight"));
+    }
+
+
+    [ContextMenu("Check Flashlight")]
+    private void TestCheckPermanent()
+    {
+        Debug.Log("Has Flashlight: " + SaveManager.HasPermanentItem("flashlight"));
+    }
+
+
     [ContextMenu("Save To Disk")]
     private void TestSave()
     {
         SaveManager.SaveToDisk();
     }
 }
+
